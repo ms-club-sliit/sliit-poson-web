568b0f7d1985680fec91d5cd180d834f3a12838d
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2023-05-02 20:18:15 +0530

    created-previousWSection

--- src/views/OurWork/index.jsx
+++ src/views/OurWork/index.jsx
@@ -1,15 +1,45 @@
 import image from './../../assets/ourWork/ourWork.png';
 
 const OurWork = () => {
+  //change the details of the works to be displayed here
+  const works = [
+    {
+      title: 'Tom & Jerry',
+      thumb: 'https://poson-web.s3.ap-southeast-2.amazonaws.com/videoThumb.png',
+      vsrc: 'https://poson-web.s3.ap-southeast-2.amazonaws.com/Tom+%26+Jerry+_+Is+Jerry+Taking+Care+of+Tom_+_+Classic+Cartoon+_+WB+Kids.mp4',
+    },
+    {
+      title: 'Tom & Jerry',
+      thumb: 'https://poson-web.s3.ap-southeast-2.amazonaws.com/videoThumb.png',
+      vsrc: 'https://poson-web.s3.ap-southeast-2.amazonaws.com/Tom+%26+Jerry+_+Is+Jerry+Taking+Care+of+Tom_+_+Classic+Cartoon+_+WB+Kids.mp4',
+    },
+  ];
   return (
-    <div className='flex flex-col items-center z-0  h-screen bg-gradient-to-l from-fuchsia-200 to-fuchsia-100 dark:bg-gradient-to-l dark:from-gray-950 dark:to-slate-900'>
-      <img src={image} alt='OurWorksBanner' className='z-0' />
+    <div className='z-0 flex flex-col items-center  bg-gradient-to-l from-fuchsia-200 to-fuchsia-100 dark:bg-gradient-to-l dark:from-slate-950 dark:to-slate-900'>
+      <img src={image} alt='OurWorksBanner' className='mt-0  w-full' />
 
-      <div className=' absolute mt-20 w-9/12 h-full z-1 rounded-3xl bg-gradient-to-t from-fuchsia-300 to-fuchsia-50 dark:bg-gradient-to-t dark:from-black dark:to-slate-950 '>
-        <div className='flex text-center '>
-          <p>Our</p>
-          <p>previous works</p>
-        </div>
+      <div className='flex flex-col items-center -mt-24 md:-mt-40 xl:-mt-80 mb-16 lg:mb-28 w-9/12  rounded-3xl bg-gradient-to-t from-fuchsia-200 to-white dark:bg-gradient-to-t dark:from-black dark:to-slate-950 '>
+        <h1 className='flex mt-10 lg:mt-24 mb-10  text-sm lg:text-3xl font-bold  dark:text-white'>
+          <div className='underline underline-offset-8 decoration-amber-300'>
+            Our&nbsp;
+          </div>
+          Previous Works
+        </h1>
+        {works.map((work) => (
+          <div className='flex flex-col items-center  mb-10 lg:mb-28 '>
+            <p className='w-8/12 text-xs lg:text-xl dark:text-white'>
+              &nbsp;{work.title}
+            </p>
+            <video
+              class='w-8/12 rounded-xl mt-1 lg:mt-4'
+              poster={work.thumb}
+              controls
+            >
+              <source src={work.vsrc} type='video/mp4' />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        ))}
       </div>
     </div>
   );
