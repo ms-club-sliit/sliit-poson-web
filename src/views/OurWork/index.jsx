3a5f1c22e5cbdd7c790200c45884d62615c9a872
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2023-05-02 09:21:09 +0530

    created-containers

--- src/views/OurWork/index.jsx
+++ src/views/OurWork/index.jsx
@@ -1,10 +1,16 @@
-/**
- * Our Work Section
- */
+import image from './../../assets/ourWork/ourWork.png';
+
 const OurWork = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
-      <h1 className="text-3xl font-bold underline dark:text-white">Our Work</h1>
+    <div className='flex flex-col items-center z-0  h-screen bg-gradient-to-l from-fuchsia-200 to-fuchsia-100 dark:bg-gradient-to-l dark:from-gray-950 dark:to-slate-900'>
+      <img src={image} alt='OurWorksBanner' className='z-0' />
+
+      <div className=' absolute mt-20 w-9/12 h-full z-1 rounded-3xl bg-gradient-to-t from-fuchsia-300 to-fuchsia-50 dark:bg-gradient-to-t dark:from-black dark:to-slate-950 '>
+        <div className='flex text-center '>
+          <p>Our</p>
+          <p>previous works</p>
+        </div>
+      </div>
     </div>
   );
 };
