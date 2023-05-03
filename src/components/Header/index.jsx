9b0c87e99f2f83ddef85d9ef003d1ef149579fe4
Ravindu-Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2023-05-03 15:04:26 +0530

    navigation added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,29 @@
 import { useState } from 'react';
 
 const Header = () => {
-  const [activeLink, setActiveLink] = useState(null);
+  const [activeLink, setActiveLink] = useState('home');
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
   };
 
   return (
-    <nav className="fixed z-999 bg-gray-50 shadow-lg rounded-full inline-flex">
+    <nav className="fixed z-999 bg-gray-50 shadow-lg rounded-full inline-flex flex justify-center">
       <div className="mx-auto px-4 py-3 flex items-center justify-between relative">
         <div className="flex items-center">
-          <a href="#" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'home' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('home')}>
+          <a href="#home" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'home' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('home')}>
             Home
           </a>
-          <a href="#" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'mentors' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('mentors')}>
+          <a href="#mentors" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'mentors' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('mentors')}>
             Our Mentors
           </a>
-          <a href="#" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'team' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('team')}>
+          <a href="#members" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'team' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('team')}>
             Our Team
           </a>
-          <a href="#" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'work' ? 'bg-purple-900 dark:bg-gray-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('work')}>
+          <a href="#ourwork" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'work' ? 'bg-purple-900 dark:bg-gray-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('work')}>
             Our Work
           </a>
-          <a href="#" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'contact' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('contact')}>
+          <a href="#contactus" className={`font-normal text-gray-800 font-bold mr-4 ${activeLink === 'contact' ? 'bg-purple-900 dark:bg-gray-900 text-white rounded-full px-6 py-2' : ''}`} onClick={() => handleLinkClick('contact')}>
             Contact Us
           </a>
         </div>
