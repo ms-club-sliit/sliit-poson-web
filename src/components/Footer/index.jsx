db3ca3c35a2a2eb67f8e774bfd374db5cc83e565
IT21375132 <gokulabisheak12@gmail.com>
2023-05-04 22:05:16 +0530

    Added new logos to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,15 @@
-import msclublogo from '../../assets/ms_club_logo.png'
+import msclublogo from '../../assets/footer/ms_club_logo.png'
+import imlogo from '../../assets/footer/im_logo.png'
+import imsclogo from '../../assets/footer/imsc_logo.png'
 
 const Footer = () => {
   return (
     <>
-      <div className='grid md:grid-cols-3 flex items-center text-center bg-white py-10 dark:bg-black dark:text-white sm:grid-cols-1'>
-        <div className='flex justify-center items-center px-4'>
-          <img className='w-20 h-auto' src={msclublogo} />
+      <div className='grid grid-cols-3 flex items-center text-center bg-neutral-50 py-10 dark:bg-black dark:text-white max-lg:grid-cols-1'>
+        <div className='flex space-x-8 justify-center items-center px-4'>
+          <img className='h-12 w-auto' src={imlogo} />
+          <img className='h-12 w-auto' src={imsclogo} />
+          <img className='h-12 w-auto' src={msclublogo} />
         </div>
         <div className='flex justify-center items-center px-4 py-4 '>
           Designed and Developed by MS Club of SLIIT
